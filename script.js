31c2f69d6f449620550f16acd975325d7149bb66
Demontier <demontier@outlook.com>
2021-06-14 23:26:03 -0300

    finalizado

--- script.js
+++ script.js
@@ -1,89 +1,79 @@
-function setTextImg () {
-  const text = document.querySelector('#text-input')
-  const memeText = document.querySelector('#meme-text')
+const memeImg = document.querySelector('#meme-image');
+const colorBorder = document.querySelector('#meme-image-container');
+
+function setTextImg() {
+  const text = document.querySelector('#text-input');
+  const memeText = document.querySelector('#meme-text');
   text.addEventListener('keyup', () => {
-    const newText = text.value
-    memeText.innerHTML = newText
-  })
+    const newText = text.value;
+    memeText.innerHTML = newText;
+  });
 }
 setTextImg();
 
 function selectImg() {
-  const test1 = document.querySelector('#meme-insert')
-  const test = document.querySelector('#meme-image')
-  test1.addEventListener('change', (event) => {
-    test.src = URL.createObjectURL(event.target.files[0]);
-  })
+  const selectMeme = document.querySelector('#meme-insert');
+  selectMeme.addEventListener('change', (event) => {
+    memeImg.src = URL.createObjectURL(event.target.files[0]);
+  });
 }
 selectImg();
 
 function buttonFire() {
-  const fire = document.querySelector('#fire')
-  fire.style = "background: red;"
+  const fire = document.querySelector('#fire');
+  fire.style = 'background: red;';
   fire.addEventListener('click', () => {
-    
-    const colorFire = document.querySelector('#meme-image-container');
-    colorFire.style = "border: 3px dashed red;"
-  })
+    colorBorder.style = 'border: 3px dashed red;';
+  });
 }
 buttonFire();
 
 function buttonWater() {
-  const water = document.querySelector('#water')
-  water.style = "background: blue;"
+  const water = document.querySelector('#water');
+  water.style = 'background: blue;';
   water.addEventListener('click', () => {
-    
-    const colorWater = document.querySelector('#meme-image-container');
-    colorWater.style = "border: 5px double blue;"
-  })
+    colorBorder.style = 'border: 5px double blue;';
+  });
 }
 buttonWater();
 
 function buttonEarth() {
-  const earth = document.querySelector('#earth')
-  earth.style = "background: green;"
+  const earth = document.querySelector('#earth');
+  earth.style = 'background: green;';
   earth.addEventListener('click', () => {
-    
-    const colorEarth = document.querySelector('#meme-image-container');
-    colorEarth.style = "border: 6px groove green;"
-  })
+    colorBorder.style = 'border: 6px groove green;';
+  });
 }
 buttonEarth();
 
 function selectMeme1() {
-  const test1 = document.querySelector('#meme-1')
-  const test = document.querySelector('#meme-image')
-  test1.addEventListener('click', () => {
-    test.src="/imgs/meme1.png"
-  })
+  const selecMeme1 = document.querySelector('#meme-1');
+  selecMeme1.addEventListener('click', () => {
+    memeImg.src = '/imgs/meme1.png';
+  });
 }
 selectMeme1();
 
 function selectMeme2() {
-  const test1 = document.querySelector('#meme-2')
-  const test = document.querySelector('#meme-image')
-  test1.addEventListener('click', () => {
-    test.src="/imgs/meme2.png"
-  })
+  const selecMeme2 = document.querySelector('#meme-2');
+  selecMeme2.addEventListener('click', () => {
+    memeImg.src = '/imgs/meme2.png';
+  });
 }
 selectMeme2();
 
 function selectMeme3() {
-  const test1 = document.querySelector('#meme-3')
-  const test = document.querySelector('#meme-image')
-  test1.addEventListener('click', () => {
-    test.src="/imgs/meme3.png"
-  })
+  const selecMeme3 = document.querySelector('#meme-3');
+  selecMeme3.addEventListener('click', () => {
+    memeImg.src = '/imgs/meme3.png';
+  });
 }
 selectMeme3();
 
 function selectMeme4() {
-  const test1 = document.querySelector('#meme-4')
-  const test = document.querySelector('#meme-image')
-  test1.addEventListener('click', () => {
-    test.src="/imgs/meme4.png"
-  })
+  const selecMeme4 = document.querySelector('#meme-4');
+  selecMeme4.addEventListener('click', () => {
+    memeImg.src = '/imgs/meme4.png';
+  });
 }
 selectMeme4();
-
-
